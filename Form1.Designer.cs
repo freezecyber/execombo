ac6ccd5c238100849837557b507304e0232a2296
freezecyber <37415540+freezecyber@users.noreply.github.com>
2018-06-11 16:36:56 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace execombo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(727, 23);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "LA COMPAGNIE  AITEST PROJECTYJ 324891 ";
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "set dir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(75, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "scan exe";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(226, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "reload";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(652, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 66);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "open txt";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(577, 23);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 66);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "del selected";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(307, 28);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(149, 56);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "OPEN SELECTED";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(156, 59);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 20);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "all rep";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Red;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(496, 37);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 38);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "dell all";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(727, 89);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace execombo
+{
+    public partial class Form1 : Form
+    {
+        private IEnumerable<string> hsd;
+        System.Speech.Synthesis.SpeechSynthesizer say = new System.Speech.Synthesis.SpeechSynthesizer();
+        private string setdir;
+        private string[] hdf;
+        private IEnumerable<string> sdf;
+        private IEnumerable<string> ffh;
+        private IEnumerable<string> ddv;
+        private IEnumerable<string> dvp;
+        private IEnumerable<string> dbg;
+        private IEnumerable<string> fvgn;
+        private IEnumerable<string> dvgf;
+        private IEnumerable<string> dfdg;
+        private int ikk;
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists("execombo.tx"))
+            {
+                File.WriteAllText("execombo.tx", "");
+            }
+            if (!File.Exists("execombo2.tx"))
+            {
+                File.WriteAllText("execombo2.tx", "");
+            }
+            Application.DoEvents(); Thread.Sleep(0);
+            comboBox1.Items.Clear();
+            setdir = Directory.GetCurrentDirectory();
+            string[] dpf = File.ReadAllLines("execombo2.tx");
+           dfdg= dpf.Distinct();
+            foreach (string fjf in dfdg)
+            {
+                try
+                {
+                    if (fjf.Contains("vshost"))
+                    { }
+                    else
+                    {
+                        string dhf = Path.GetPathRoot(fjf);
+                        string fhjg = Path.GetFileName(dhf + fjf);
+                        comboBox1.Items.Add(dhf + fhjg);
+                    }
+                }
+                catch { }
+            }
+            Application.DoEvents(); Thread.Sleep(0);
+            say.SpeakAsync("it is good all done");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog sjdf = new FolderBrowserDialog();
+            Application.DoEvents();
+            sjdf.ShowDialog();
+            Application.DoEvents();
+
+            setdir = sjdf.SelectedPath; 
+            say.SpeakAsync("all done directory set");
+            comboBox1.Items.Clear();
+
+            string[] dpf = File.ReadAllLines("execombo2.tx");
+           sdf= dpf.Distinct();
+            foreach (string fjf in sdf)
+            {
+                try
+                {
+                    if (fjf.Contains("vshost"))
+                    {
+                    }
+                    else
+                    {
+                        string dhf = Path.GetPathRoot(fjf);
+                        string fhjg = Path.GetFileName(dhf + fjf);
+                        comboBox1.Items.Add(fhjg);
+                    }
+                }
+                catch { }
+            }
+            Application.DoEvents(); Thread.Sleep(0);
+            say.SpeakAsync("it is good all done");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkBox1.Checked)
+                {
+                    hdf = Directory.GetFiles(setdir, "*.exe", SearchOption.AllDirectories);
+                }
+                else
+                {
+                    hdf = Directory.GetFiles(setdir, "*.exe");
+                }
+            }
+            catch { }
+            say.SpeakAsync("get file");
+            foreach (string fhjg in hdf)
+            {
+                try
+                {
+                    if (fhjg.Contains("vshost"))
+                    {
+                    }
+                    else
+                    {
+                        string fht = Path.GetFileName(fhjg);
+                        comboBox1.Items.Add(setdir + fht);
+                        File.AppendAllText("execombo.tx", fhjg + Environment.NewLine);
+
+                    }
+                }
+                catch { }
+            }
+            say.SpeakAsync("file found organize type");
+            string[] fhty = File.ReadAllLines("execombo.tx");
+          ffh=  fhty.Distinct(); Application.DoEvents(); Thread.Sleep(0);
+            foreach (string hdfgf in ffh)
+            {
+                if (hdfgf.Contains("vshost"))
+                {
+                }
+                else
+                {
+                    File.AppendAllText("execombo2.tx", hdfgf + Environment.NewLine);
+                }
+            }
+            say.SpeakAsync("all done file set");
+            button3_Click(null, null);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+
+            string[] dpf = File.ReadAllLines("execombo2.tx");
+           ddv= dpf.Distinct();
+            foreach (string fjf in ddv)
+            {
+                try
+                {
+                    if (fjf.Contains("vshost"))
+                    {
+                    }
+                    else
+                    {
+                        string hdf = Path.GetPathRoot(fjf);
+                        string fhjg = Path.GetFileName(fjf);
+                        comboBox1.Items.Add(hdf + fhjg);
+                    }
+                }
+                catch { }
+            }
+            Application.DoEvents(); Thread.Sleep(0);
+            say.SpeakAsync("it is good all done");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            say.SpeakAsync("starting");
+            Process.Start("execombo2.tx"); Application.DoEvents(); Thread.Sleep(0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                say.SpeakAsync("start deleting");
+                string[] psnbf = File.ReadAllLines("execombo2.tx");
+                Application.DoEvents(); Thread.Sleep(0);
+               dvgf= psnbf.Distinct();
+                foreach (string dhf in dvgf)
+                {
+                    string shdf = Path.GetFileName(dhf);
+                    if (comboBox1.GetItemText(comboBox1.SelectedItem).Contains(shdf))
+                    {
+                        try
+                        {
+                            if (dhf.Contains("vshost"))
+                            {
+                            }
+                            else
+                            {
+                                string fht = File.ReadAllText("execombo2.tx");
+
+                                string hdgf = fht.Replace(dhf, "");
+                                File.WriteAllText("execombo2.tx", hdgf);
+                                comboBox1.Items.Remove(comboBox1.GetItemText(comboBox1.SelectedItem));
+                                Application.DoEvents(); Thread.Sleep(0);
+                            }
+                        }
+                        catch { }
+                    }
+
+                }
+                comboBox1.Items.Clear();
+
+                string[] dpf = File.ReadAllLines("execombo2.tx");
+               fvgn= dpf.Distinct();
+                foreach (string fjf in fvgn)
+                {
+                    try
+                    {
+                        if (fjf.Contains("vshost"))
+                        {
+                        }
+                        else
+                        {
+                            string dhf = Path.GetPathRoot(fjf);
+                            string fhjg = Path.GetFileName(dhf + fjf);
+                            comboBox1.Items.Add(fhjg);
+                        }
+                    }
+                    catch { }
+                }
+                Application.DoEvents(); Thread.Sleep(0);
+                say.SpeakAsync("it is good all done");
+            }
+            catch { say.SpeakAsync("error with deleting error"); }
+            say.SpeakAsync("finish deleted"); Application.DoEvents(); Thread.Sleep(0);
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ikk = 1;
+                say.SpeakAsync("ok ouverture");
+                string[] pfg = File.ReadAllLines("execombo2.tx");
+               dbg=  pfg.Distinct();
+                foreach (string dhf in dbg)
+                {
+
+                    string shdf = Path.GetFileName(dhf);
+                    if (comboBox1.GetItemText(comboBox1.SelectedItem).Contains(shdf))
+                    {
+                        if (ikk == 1)
+                        {
+                            if (shdf.Contains("vshost"))
+                            {
+                            }
+                            else
+                            {
+                                ikk++;
+                                say.SpeakAsync("yes");
+                                Process.Start(dhf); Application.DoEvents(); Thread.Sleep(0);
+                            }
+                        }
+                    }
+
+                }
+                 say.SpeakAsync("it is ouvert");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ikk = 1;
+
+            say.SpeakAsync("ok ouverture");
+            string[] pfg = File.ReadAllLines("execombo2.tx");
+           dvp= pfg.Distinct();
+            foreach (string dhf in dvp)
+            {
+                string shdf = Path.GetFileName(dhf);
+                if (comboBox1.GetItemText(comboBox1.SelectedItem).Contains(shdf))
+                {
+                    if (ikk == 1)
+                        if (dhf.Contains("vshost"))
+                        {
+                        }
+                        else
+                        {
+                            ikk++;
+                            say.SpeakAsync("yes");
+                            Process.Start(dhf); Application.DoEvents(); Thread.Sleep(0);
+                        }
+                }
+            }
+
+            
+             say.SpeakAsync("it is done ouvert");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("execombo2.tx", "");
+        }
+    }
+}
